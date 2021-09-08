38f62e218e621f5f8fcb7da58c5147a49eeae761
aggarabedian <abrahamgarabedian@gmail.com>
2021-09-08 14:34:03 -0700

    Populates template with data from db

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.Home.as_view(), name='home'),
   path('about/', views.About.as_view(), name='about'),
-  # path('journals/', views.Journals.as_view(), name='journals'),
+  path('journals/', views.JournalList.as_view(), name='journals'),
+  path('memory/<int:pk>/', views.MemoryView.as_view(), name='memory'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
+
+
+# Imports Models
+from .models import Journal, Memory
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -11,3 +18,18 @@ class Home(TemplateView):
 class About(TemplateView):
   template_name = "about.html"
 
+class JournalList(TemplateView):
+  template_name = "journal_list.html"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["journals"] = Journal.objects.all()
+    return context
+
+class MemoryView(TemplateView):
+  template_name = "memory.html"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["memories"] = Memory.objects.all()
+    return context
